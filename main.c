c8e72715cf2babfcfc60dd75579b9bdb021389f2
bledixhindi <54402850+bledixhindi@users.noreply.github.com>
2022-03-21 21:56:18 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,v[10][10],sum,n;
+    
+    printf("Vendos n:");
+    scanf("%d",&n);
+    
+    printf("mbush:");
+    
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("Vendos vleren ne matricat v[%d][%d]:",i,j);
+            scanf("%d",&v[i][j]);
+        }
+    }
+    
+    printf("Matricat jane:\n");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%d\t",v[i][j]);
+        }
+        printf("\n");
+    }
+    
+    for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(i==j)
+			{
+				sum=sum+v[i][j];
+			}
+		}
+		
+		
+	}
+		printf("Shuma eshte :%d \t",sum);
+		
+		return 0;
+}
